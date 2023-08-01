9cf62b96bb62921efa78311cb4ac49d3fe1ce7e9
Zachary Cutlip <uid000@gmail.com>
2023-08-01 15:27:58 -0700

    add __about__.py

---
+++ src/fancontrol/__about__.py
@@ -0,0 +1,30 @@
+__title__ = "rpi-gpio-fancontrol"
+__version__ = "1.0.0"
+__summary__ = "A python utility to control a variable speed fan on a Raspberry Pi"
+
+"""
+See PEP 440 for version scheme
+https://www.python.org/dev/peps/pep-0440/#examples-of-compliant-version-schemes
+Examples:
+
+FINAL
+0.9
+0.9.1
+0.9.2
+...
+0.9.10
+0.9.11
+1.0
+1.0.1
+1.1
+2.0
+2.0.1
+...
+
+PRE_RELEASES
+
+X.YaN   # Alpha release
+X.YbN   # Beta release
+X.YrcN  # Release Candidate
+X.Y     # Final release
+"""
